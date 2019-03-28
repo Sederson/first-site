e6dacc23e9678642c38238cd2f211e1cb25f5183
Sederson <49036328+Sederson@users.noreply.github.com>
2019-03-28 18:05:29 +0200

    Added files to repository.

---
+++ mine.js
@@ -0,0 +1,7 @@
+function showMenu () {
+  document.querySelector('.head__ul').classList.add('active');
+}
+
+function closeMenu () {
+  document.querySelector('.head__ul').classList.remove('active');
+}
